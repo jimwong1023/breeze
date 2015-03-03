fa427d1c213f2e1901aaf6f916cb2dc8fef64751
Jimmy <jimwong1023@gmail.com>
2015-03-03 08:54:09 -0800

    nil guards against no date input for search

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,7 +7,7 @@ class HomepageController < ApplicationController
 
   private
     def available_query
-      if params[:available]
+      if params[:available] && (params[:available][:search_date] != "")
         if tracking = cars_available_on(params[:available][:search_date])
           @available = tracking.available_cars
         else
@@ -17,7 +17,7 @@ class HomepageController < ApplicationController
     end
 
     def utilization_query
-      if params[:utilization]
+      if params[:utilization] && (params[:utilization][:search_date_end] != "") && (params[:utilization][:search_date_start] != "")
         results = cars_available_between(params[:utilization][:search_date_start], params[:utilization][:search_date_end])
         if results.first["available"]
           @utilization = ((results.first["total"].to_i - results.first["available"].to_i).to_f / results.first["total"].to_f)
